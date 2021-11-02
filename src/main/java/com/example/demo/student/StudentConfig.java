0ade68fb68d9c8e681f9ccf60056852ddf72beed
Gabriel Santana <gabriel.gsantana7@gmail.com>
2021-11-01 22:35:05 -0300

    feat: Add command line runner to seed database

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StudentConfig {
+  
+  @Bean
+  CommandLineRunner commandLineRunner(StudentRepository repository) {
+    return args -> {
+      Student john = new Student("John", "john@gmail.com", LocalDate.of(2000, Month.JANUARY, 5));
+      Student maria = new Student("Maria", "maria@gmail.com", LocalDate.of(2000, Month.JANUARY, 5));
+
+      repository.saveAll(List.of(john, maria));
+    };
+  }
+}
